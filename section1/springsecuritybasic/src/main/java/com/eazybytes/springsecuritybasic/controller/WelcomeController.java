e1baad1cfbfd6f154c229a952c7931cc12d20cd8
pedro.neri <neri.pedro@icloud.com>
2023-10-04 21:43:46 -0300

    teste in dev

--- section1/springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
+++ section1/springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/welcome-to")
     public String sayWelcome() {
         return "Welcome to Spring Application with Security";
     }
