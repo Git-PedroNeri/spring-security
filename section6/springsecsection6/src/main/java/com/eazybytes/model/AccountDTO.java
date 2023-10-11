c32c5ac9fd1096adfe36570fa6f854df9eeeec49
pedro.neri <neri.pedro@icloud.com>
2023-10-11 20:22:07 -0300

    updates

---
+++ section6/springsecsection6/src/main/java/com/eazybytes/model/AccountDTO.java
@@ -0,0 +1,15 @@
+package com.eazybytes.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+
+/**
+ * @author pedro.neri
+ * @project springsecsection6
+ * @email pedro.neri@
+ * @createat 11/10/2023
+ */
+
+@Builder
+public class AccountDTO extends Accounts {
+}
